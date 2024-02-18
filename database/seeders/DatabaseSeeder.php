6aef1e3d4877b49ef24d95c176e90d60a5d2de90
Fabian Rodriguez <fabianrodriguezbrochero98@gmail.com>
2024-02-18 02:13:12 -0500

    dsgDSA

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
    
-        $this->call(nuevoSeeder::class);
+        $this->call(NuevoSeeder::class);
         //$this->call(EventSeeder::class);
     }
 }
