1aeca0e94ad610b0413a0e7676cf09bd4f03bf11
Fabian Rodriguez <fabianrodriguezbrochero98@gmail.com>
2024-02-28 12:18:36 -0500

    url2

--- resources/views/emails/formjob.blade.php
+++ resources/views/emails/formjob.blade.php
@@ -4,11 +4,11 @@
     
 </head>
 <body>
-    <h2>{{ $data['fullname'] }} Quiere contactar contigo para postularse como Walker</h2>
+    <h2>{{ $data['name'] }} Quiere contactar contigo para postularse como Walker</h2>
     <p>Hola, se ha generado el contacto de un candidato con los siguientes datos:</p>
     <ul>
         <ul>
-            <li><strong>Full Name:</strong> {{ $data['fullname'] }}</li>
+            <li><strong>Full Name:</strong> {{ $data['name'] }}</li>
             <li><strong>Address:</strong> {{ $data['address'] }}</li>
             <li><strong>Email:</strong> {{ $data['email'] }}</li>
             <li><strong>Phone:</strong> {{ $data['phone'] }}</li>
