81bff990064b1daa5ac15306731befda87fa7f56
algomelo <fabianrodriguezbrochero98@gmail.com>
2024-09-20 17:11:22 -0500

    ajuste correo registro

--- resources/views/emails/notificationRegistro.blade.php
+++ resources/views/emails/notificationRegistro.blade.php
@@ -7,7 +7,11 @@
     <p>Hola, un nuevo usuario se ha registrado en sus sistema</p>
     <ul>
         <ul>
-   
+            <li><strong>Name:</strong> {{ $data['name'] }}</li>
+            <li><strong>Email:</strong> {{ $data['email'] }}</li>
+            <li><strong>Phone:</strong> {{ $data['phone'] }}</li>
+            <li><strong>Address:</strong> {{ $data['address'] }}</li>
+            <li><strong>Dog Name:</strong> {{ $data['petname'] }}</li>
         </ul>
     </ul>
 </body>
